b5d107863cc8f6943166b466175835216165f281
Nicolas Gonzalez <nicolas.gonzalez.alexi@gmail.com>
2021-11-28 21:46:14 -0300

    Configuracion de setting y creacion de core + app/blog + tailwind + postgres

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView, View
+from django.shortcuts import redirect, render, get_list_or_404, redirect
+from django.core.paginator import Paginator
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        context={
+
+        }
+        return render(request, 'pages/index.html', context)
